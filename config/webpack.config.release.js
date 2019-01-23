07acb2a34d51103b179970d5c11a30b07481bf1c
Valentin Ledrapier <vledrapier@wanadev.fr>
2019-01-23 17:26:53 +0100

    Replace uglifyjs with terser

--- config/webpack.config.release.js
+++ config/webpack.config.release.js
@@ -2,7 +2,7 @@ const webpack = require("webpack");
 const merge = require("webpack-merge");
 const path = require("path");
 
-const UglifyJsWebpackPlugin = require("uglifyjs-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const CompressionWebpackPlugin = require("compression-webpack-plugin");
 const ProgressBarWebpackPlugin = require("progress-bar-webpack-plugin");
@@ -36,7 +36,7 @@ module.exports = merge(common, {
             },
         },
         minimizer: [
-            new UglifyJsWebpackPlugin({
+            new TerserWebpackPlugin({
                 cache: true,
                 parallel: true,
                 sourceMap: true,
