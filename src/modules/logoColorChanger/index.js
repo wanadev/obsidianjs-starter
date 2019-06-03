7e42f8db5461a5c89c5d9e9ea52bbf401f4b1c00
ldemicheli@wanadev.fr <ldemicheli@wanadev.fr>
2019-06-03 17:21:47 +0200

    clean

--- src/modules/logoColorChanger/index.js
+++ src/modules/logoColorChanger/index.js
@@ -2,7 +2,6 @@ export default {
     name: "logo-color-changer",
     requires: ["logo"],
     load() {
-        console.log("coucou");
         const LogoColorChangerController = require("./src/logoColorChangerController").default;
 
         return new LogoColorChangerController();
